472e586801cd367ccd8c98b61e4d676741e6717e
GitMoDu <batatas@gmail.com>
2021-07-24 19:18:38 +0100

    Added Classed example.

---
+++ examples/ExampleDriver/ExampleDriver.ino
@@ -0,0 +1,67 @@
+//
+// Example of classed Driver, using the interface callbacks.
+// Reads and Writes in Half-Duplex mode, avoiding collisions.
+//
+
+#define DEBUG_LOG
+
+#include "ExampleDriverClass.h"
+
+const uint8_t MaxPacketSize = 8;
+const uint8_t TestPacketSize = 8;
+const uint8_t ReadPin = 2;
+const uint8_t WritePin = 7;
+
+ExampleDriverClass<MaxPacketSize, ReadPin, WritePin> Driver;
+
+const uint32_t SendPeriodMillis = 500;
+uint32_t LastSent = 0;
+
+
+void setup()
+{
+#ifdef DEBUG_LOG
+	Serial.begin(9600);
+#endif
+
+	attachInterrupt(digitalPinToInterrupt(ReadPin), OnReaderPulse, RISING);
+	Driver.Start();
+
+#ifdef DEBUG_LOG
+	Serial.println(F("ExampleDriver Start."));
+#endif
+}
+
+void loop()
+{
+	if (millis() - LastSent > SendPeriodMillis)
+	{
+		LastSent = millis();
+#ifdef DEBUG_LOG
+		Serial.print(F("Sending Packet @"));
+		Serial.print(micros());
+		Serial.println(F(" us"));
+#endif
+
+		// Fill in test data.
+		for (uint8_t i = 0; i < TestPacketSize; i++)
+		{
+			Driver.OutgoingPacket[i] = i + 1;
+		}
+		Driver.SendPacket(TestPacketSize);
+	}
+	else
+	{
+		Driver.Check();
+	}
+}
+
+void OnReaderPulse()
+{
+	Driver.OnReaderInterrupt();
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+	Driver.OnWriterInterrupt();
+}
---
+++ examples/ExampleDriver/ExampleDriverClass.h
@@ -0,0 +1,137 @@
+// ExampleDriverClass.h
+
+#ifndef _EXAMPLEDRIVERCLASS_h
+#define _EXAMPLEDRIVERCLASS_h
+
+#include <PulseIntervalModulator.h>
+
+template<const uint8_t MaxPacketSize, const uint8_t ReadPin, const uint8_t WritePin>
+class ExampleDriverClass : virtual public PacketReaderCallback, virtual public PacketWriterCallback
+{
+private:
+	volatile bool PacketLostFlag = false;
+	volatile bool PacketReceivedFlag = false;
+	volatile bool PacketSentFlag = false;
+
+	uint8_t IncomingPacketSize = 0;
+	uint32_t IncomingStartTimestamp = 0;
+	uint8_t IncomingPacket[Constants::MaxDataBytes];
+
+	PacketReader<MaxPacketSize, ReadPin> Reader;
+	PacketWriter<MaxPacketSize, WritePin> Writer;
+
+public:
+	uint8_t OutgoingPacket[MaxPacketSize];
+
+public:
+	ExampleDriverClass()
+		: PacketReaderCallback()
+		, PacketWriterCallback()
+		, Reader(IncomingPacket)
+		, Writer()
+	{
+	}
+
+	void OnWriterInterrupt()
+	{
+		Writer.OnWriterInterrupt();
+	}
+
+	void OnReaderInterrupt()
+	{
+		Reader.OnPulse();
+	}
+
+	// Must perform interrupt attach before starting.
+	// attachInterrupt(digitalPinToInterrupt(ReadPin), OnPulse, RISING);
+	void Start()
+	{
+		Reader.Start(this);
+		Writer.Start(this);
+	}
+
+	const bool CanSend()
+	{
+		return Reader.CanSend();
+	}
+
+	// Must check with CanSend() right before this call.
+	void SendPacket(const uint8_t packetSize)
+	{
+		// Blank reader to ignore cross-talk.
+		Reader.BlankReceive();
+		Writer.SendPacket(BufferOut, packetSize);
+	}
+
+	void Check()
+	{
+		if (PacketLostFlag)
+		{
+			PacketLostFlag = false;
+#ifdef DEBUG_LOG
+			Serial.print(F("OnPacketLost @"));
+			Serial.print(IncomingStartTimestamp);
+			Serial.println(F(" us"));
+#endif
+		}
+		else if (PacketReceivedFlag)
+		{
+			PacketReceivedFlag = false;
+
+			uint8_t incomingSize = 0;
+
+			if (Reader.HasIncoming(incomingSize))
+			{
+				// When packet is available.
+#ifdef DEBUG_LOG
+				Serial.print(F("OnPacketReceived @"));
+				Serial.print(IncomingStartTimestamp);
+				Serial.print(F(" us ("));
+				Serial.print(incomingSize);
+				Serial.println(F(") bytes"));
+
+				Serial.print(F("Data: "));
+				for (uint8_t i = 0; i < incomingSize; i++)
+				{
+					Serial.print("|");
+					Serial.print(IncomingPacket[i], HEX);
+				}
+				Serial.println('|');
+				Serial.println();
+#endif
+			}
+
+			// Restore Reader after consuming the packet.
+			Reader.Restore();
+		}
+		else if (PacketSentFlag)
+		{
+			PacketSentFlag = false;
+#ifdef DEBUG_LOG
+			Serial.print(F("OnPacketSent @"));
+			Serial.print(micros());
+			Serial.println(F(" us"));
+#endif
+		}
+	}
+
+	virtual void OnPacketReceived(const uint32_t startTimestamp)
+	{
+		PacketReceivedFlag = true;
+		IncomingStartTimestamp = startTimestamp;
+	}
+
+	virtual void OnPacketLost(const uint32_t startTimestamp)
+	{
+		PacketLostFlag = true;
+		IncomingStartTimestamp = startTimestamp;
+	}
+
+	virtual void OnPacketSent()
+	{
+		// Restore Reader after blanking during sending.
+		Reader.Restore();
+		PacketSentFlag = true;
+	}
+};
+#endif
